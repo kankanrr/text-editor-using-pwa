33563ae304d7447377851d26c35dd76671ae971f
Sameer Mirza <srmirza14@gmail.com>
2023-07-19 20:37:54 -0400

    completed modules

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -50,7 +50,23 @@ module.exports = () => {
     ],
 
     module: {
-      rules: [],
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          // babel
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
+            },
+          },
+        },
+      ],
     },
   };
 };
